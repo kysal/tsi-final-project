90498c1582039f0641baa9385a50986910d3b813
Kyle <kylesalisbury2@gmail.com>
2022-12-19 10:49:06 +0000

    Added home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 function HomePage() {
@@ -5,7 +6,28 @@ function HomePage() {
 
     return (
         <div>
-            This is the Home Page!
+            <table className="tab-holder">
+                <tbody>
+                    <tr>
+                        <td className="tab">
+                            <div className="tab-content">
+                                <h2>Log In</h2>
+                                <Link to='/login'>
+                                    <button title="login button" className="button-primary">Log In</button>
+                                </Link>
+                            </div>
+                        </td>
+                        <td className="tab">
+                        <div className="tab-content">
+                            <h2>See All Films</h2>
+                            <Link to="/filmlist">
+                                <button title='film list button' className="button-primary">Film List</button>
+                            </Link>
+                        </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
